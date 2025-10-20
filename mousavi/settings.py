7bf03686b4878203c6fa138bca45b7b4fafd382f
Amin <Amindalvand97@gmail.com>
2025-10-20 16:33:42 +0330

    test

--- mousavi/settings.py
+++ mousavi/settings.py
@@ -114,12 +114,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # مسیر استاتیک برای جمع‌آوری فایل‌ها
 
-# Media files (uploads)
+# مسیرهای استاتیک اضافی
+STATICFILES_DIRS = [BASE_DIR / 'static']  # پوشه استاتیک اضافه در پروژه (اگر وجود دارد)
+
+# تنظیمات مدیا
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'  # مسیر برای ذخیره فایل‌های آپلود شده
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
